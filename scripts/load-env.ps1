4612b241db78438520f87a8b2c23d6b630de3ea0
TERAMEDIA\rknightley <rio.knightley@tmgroup.co.uk>
2025-07-28 15:52:54 +0100

    Add new load env script

--- scripts/load-env.ps1
+++ scripts/load-env.ps1
@@ -1,9 +1,15 @@
-# load-env.ps1
-$envLines = Get-Content ".env"
-foreach ($line in $envLines) {
-    if ($line -match "^\s*([^#][\w]+)\s*=\s*(.+)$") {
-        $key = $matches[1]
-        $value = $matches[2]
-        Set-Item -Path "Env:$key" -Value $value
-    }
+if (!(Test-Path ".env")) {
+  Write-Host ".env file not found. Exiting..."
+  exit 1
+}
+Write-Host "Environment loaded from .env"
+$env:AWS_PROFILE = (Get-Content ".env" | Where-Object { $_ -match "^AWS_PROFILE=" }) -replace "AWS_PROFILE=", ""
+$env:USER_INITIALS = (Get-Content ".env" | Where-Object { $_ -match "^USER_INITIALS=" }) -replace "USER_INITIALS=", ""
+if (-not $env:AWS_PROFILE) {
+  Write-Host "AWS_PROFILE not set in .env. Exiting..."
+  exit 1
+}
+if (-not $env:USER_INITIALS) {
+  Write-Host "USER_INITIALS not set in .env. Exiting..."
+  exit 1
 }
