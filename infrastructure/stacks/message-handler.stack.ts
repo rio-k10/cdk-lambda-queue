bc2a2e7a051c4e85d1d2e5505a338d11b596f226
TERAMEDIA\rknightley <rio.knightley@tmgroup.co.uk>
2025-07-30 14:16:26 +0100

    Add cleanup helper for untracked resources

--- infrastructure/stacks/message-handler.stack.ts
+++ infrastructure/stacks/message-handler.stack.ts
@@ -1,8 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as path from 'path';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 interface Props extends StackProps {
   namePrefix: string;
@@ -17,17 +18,18 @@ export class MessageHandlerStack extends Stack {
       this,
       `${namePrefix}SimpleMessageHandlerLambda`,
       {
-        functionName: namePrefix + 'message-handler-lambda',
+        functionName: `${namePrefix}message-handler-lambda`,
         runtime: lambda.Runtime.NODEJS_18_X,
         handler: 'index.handler',
         code: lambda.Code.fromAsset(
           path.join(__dirname, '../../services/message-handler')
-        )
+        ),
+        logRetention: logs.RetentionDays.ONE_WEEK
       }
     );
 
     new apigateway.LambdaRestApi(this, `${namePrefix}SimpleMessageGateway`, {
-      restApiName: namePrefix + 'SimpleMessageGateway',
+      restApiName: `${namePrefix}SimpleMessageGateway`,
       handler,
       deployOptions: {
         stageName: 'dev'
